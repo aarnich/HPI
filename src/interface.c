ece473ba2e4337a69d352e1bbededea65ddee54e
aarnich <aarn.gmpc@gmail.com>
2022-06-03 18:55:08 +0800

    init src

---
+++ src/interface.c
@@ -0,0 +1,22 @@
+#include "interface.h"
+void
+v_MainMenu() {
+  printf("MENU #1 User Type\n\n");
+
+  printf("D for Doctor\n");
+  printf("P for Patient\n");
+  printf("E for Exit\n");
+}
+
+void
+v_DrMenu() {
+  printf("MENU #2 Doctor\n\n");
+
+  printf("C for Create a new list of symptoms and impressions\n");
+  printf("U for Use the existing list of symptoms and impressions\n");
+  printf("D for Display symptoms\n");
+  printf("M for Modify symptoms\n");
+  printf("E for Exit\n");
+}
+
+void v_PatientMenu();
---
+++ src/utils.c
@@ -0,0 +1,37 @@
+#include "utils.h"
+
+int
+handleInt() {
+  int i;
+  while (scanf("%d", &i) != 1) {
+    printf("Error: argument %d is not an integer\n", i);
+  }
+
+  return i;
+}
+
+char
+handleCh(ReferenceInput ref) {
+  char c = '\0';
+  scanf("%c", &c);
+  while (strchr(ref, c) == NULL) {
+    printf("Error: argument %c is not a valid character\n", c);
+    scanf("%c", &c);
+  }
+
+  return c;
+}
+void
+inputHandler(TYPE inputType, ReferenceInput ref, int *input) {
+  switch (inputType) {
+    case INT:
+      *input = handleInt();
+      break;
+    case CHAR:
+      *(char *)input = handleCh(ref);
+      break;
+    default:
+      printf("Error: invalid input type\n");
+      break;
+  }
+}
